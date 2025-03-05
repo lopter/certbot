29b69cb71941ac75b085093b63258621b84bda6c
Louis Opter <louis@opter.org>
2025-03-05 22:16:09 +0000

    Relax PyOpenSSL version dependency on acme

--- acme/setup.py
+++ acme/setup.py
@@ -12,7 +12,7 @@ install_requires = [
     'josepy>=1.13.0, <2',
     # PyOpenSSL>=25.0.0 is just needed to satisfy mypy right now so this dependency can probably be
     # relaxed to >=24.0.0 if needed.
-    'PyOpenSSL>=25.0.0',
+    'PyOpenSSL>=24.0.0',
     'pyrfc3339',
     'pytz>=2019.3',
     'requests>=2.20.0',
